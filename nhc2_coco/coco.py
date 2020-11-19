c3fab1bd54329371ce7c1b824b76248c6bc20c20
fiva <Filip.Van.Ham@sofico.be>
2020-11-19 17:01:05 +0100

    minor fix for hobby account

--- nhc2_coco/coco.py
+++ nhc2_coco/coco.py
@@ -105,7 +105,7 @@ class CoCo:
                     and (response['Method'] == 'devices.status' or response['Method'] == 'devices.changed'):
                 devices = extract_devices(response)
                 for device in devices:
-                    if device['Uuid'] and self._device_callbacks[device['Uuid']]:
+                    if 'Uuid' in device and device['Uuid'] and self._device_callbacks[device['Uuid']]:
                         self._device_callbacks[device['Uuid']]['callbackHolder'](device)
 
                 return
